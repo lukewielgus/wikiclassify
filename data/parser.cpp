d49a17ef5f8053a31d4eaf338f8c3abd4372b961
Brian Faure <bfaure23@gmail.com>
2016-01-31 14:04:28 -0500

    Says I need to commit
    Says I need to commit before I can merge and get the new version of the
    files

--- data/parser.cpp
+++ data/parser.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+<<<<<<< HEAD
 #include <fstream>
 #include <vector>
 #include <string>
@@ -530,9 +531,24 @@ bool titleSearch(std::string filename, std::string target, wikiPage &inputPage){
 								}
 							}
 						}
+=======
+using std::cout;
+using std::endl;
 
-					}
+#include <fstream>
+using std::ifstream;
 
+#include <vector>
+using std::vector;
+>>>>>>> nathankjer/master
+
+#include <string>
+using std::string;
+using std::getline;
+using std::size_t;
+using std::stoi;
+
+<<<<<<< HEAD
 				}
 			}
 		}
@@ -545,101 +561,25 @@ bool titleSearch(std::string filename, std::string target, wikiPage &inputPage){
 		std::cout<<"Searched "<<counter<<" articles\n";
 		return false;
 	}
+=======
+bool isWithin(string &str, string tag1) {
+	return (str.find(tag1) != string::npos);
+>>>>>>> nathankjer/master
 }
 
-void read_file(std::string filename, std::vector<wikiPage> &input){
-  char temp[100000];
-  std::ifstream dataDump(filename);
-  bool condition=true;
-  std::string title;
-  while(condition){
-  	dataDump.getline(temp,100000);
-  	if(isTitle(temp)){
-  		title = fixTitle(temp);
-  		condition=false;
+// Parse the contents between a pair of tags
+string parse(string &str, string tag1, string tag2) {
+	size_t p1 = str.find(tag1);
+	size_t p2 = str.find(tag2, p1);
+  	if (p1 != string::npos and p2 != string::npos) {
+  		return str.substr(p1+tag1.size(), p2-p1-tag1.size());
   	}
-  }
-  int pageCount=0;
-  int validPageCount=0;
-  std::vector<std::string> titles;
-  titles.push_back(title);
-  while(validPageCount<1000000){
-  	bool condition = true;
-  	bool redirect = false;
-  	bool featured = false;
-  	bool categories = false;
-  	bool gotTimeStamp = false;
-  	bool good = false;
-  	std::vector<std::string> body;
-  	std::vector<std::string> category;
-  	std::string timeStamp;
-  	while(condition){
-  		dataDump.getline(temp, 100000);
-  		if(isTitle(temp)){
-  			std::string newTitle = fixTitle(temp);
-  			titles.push_back(newTitle);
-  			pageCount++;
-  			condition=false;
-  		}	
-  		else{
-  			if(isRedirect(temp)){
-  				redirect=true;
-  			}
-  			else{
-  				if(isFeatured(temp)){
-  					featured=true;
-  				}
-  				else{
-  					if(isCategory(temp)){
-  						std::string cat = getCategory(temp);
-  						category.push_back(cat);
-  						categories=true;
-  					}
-  					else{
-  						if(isUseless(temp)){
-  						}
-  						else{
-  							if(isTimeStamp(temp)){
-  								timeStamp = getTimeStamp(temp);
-  								gotTimeStamp = true;
-  							}
-  							else{
-  								if(isGood(temp)){
-  									good = true;
-  								}
-  								else{
-  									body.push_back(temp);
-  								}
-  							}
-  						}
-  					}
-  				}
-  			}
-  		}
+  	else {
+  		return "";
   	}
-  	wikiPage tempPage;
-  	tempPage.title = titles[pageCount-1];
-  	tempPage.body = body;
-  	if(!redirect){
-  		if(gotTimeStamp){
-  			tempPage.timeStamp = timeStamp;
-  		}
-  		if(featured){
-  			tempPage.featuredArticle=true;
-  		}
-  		if(categories){
-  			tempPage.category=category;
-  		}
-  		if(good){
-  			tempPage.goodArticle=true;
-  		}
-  		input.push_back(tempPage);
-  		validPageCount++;
-  		std::cout<<validPageCount<<"\n";
-  	}
-  }
 }
 
+<<<<<<< HEAD
 
 
 int main(){
@@ -677,17 +617,107 @@ int main(){
 	}
 	else{
 		std::cout<<"Article not found"<<std::endl;
+=======
+// Populate the given vector with all matches, in given string, between given tags
+// Returns the index immediately after last match (for purposes of block shifting)
+int parse(string &str, string tag1, string tag2, vector<string> &result) {
+	int pos = 0;                                            // Current position in string
+	while(true) {
+		size_t p1 = str.find(tag1, pos);
+		size_t p2 = str.find(tag2, p1);
+  		if (p1 != string::npos and p2 != string::npos) {    // If new match, push it back
+  			pos = p2+tag2.size();
+  			string parsed = str.substr(p1+tag1.size(), p2-p1-tag1.size());
+  			result.push_back(parsed);
+  		}
+		else {break;}                                       // Break otherwise
+>>>>>>> nathankjer/master
 	}
+	return pos;
 }
 
+class wikiPage {
+public:
+	string         title;        // Page title
+	short          ns;           // Page namespace
+	string         text;         // Page wikimarkup
+	vector<string> categories;   // Page categories
+	bool           isRedirect;   // Page redirect status
+	int            quality;      // Page quality (0: stub, 1: other, 2: good/featured)
+	string         contrib;      // Revision contributor
+	string         timestamp;    // Revision timestamp
+	
+	wikiPage(string pagestr);    // Constructor
+};
 
+// wikipage constructor
+wikiPage::wikiPage(string pagestr) {
+	title = parse(pagestr, "<title>", "</title>");
+	ns = stoi(parse(pagestr, "<ns>", "</ns>"));
+	text = parse(pagestr, "<text xml:space=\"preserve\">", "</text>");
+	parse(pagestr, "[[Category:", "]]", categories);
+	contrib = parse(pagestr, "<username>", "</username>");
+	timestamp = parse(pagestr, "<timestamp>", "</timestamp>");
+	
+	// Categorize based on text
+	isRedirect = isWithin(text, "#REDIRECT");
+	quality = 0;
+	if (isWithin(text, "{{Featured article}}") || isWithin(text, "{{Good article}}")) {
+		quality = 1;
+	}
+	
+}
 
+vector<string> getPages(string &filename, int numpages) {
+	/* Gets a vector of page strings */
 
+	ifstream dataDump(filename);                    // Create filestream
+	
+	unsigned short buffersize = 4096;               // Number of characters buffered at a time
+	unsigned blocksize = 1000000;                   // Size of string searched at a time; larger than largest article (apprx 800000)
+	string block; char buffer[buffersize];          // String objects for buffering
+	
+	vector<string> pages;                           // Initialize return value
+	unsigned long long fpos = 0;                    // Position in file
+	while(pages.size() < numpages) {                // While pages is below the specified size
+		// Load buffer into string
+		block = "";                                 // New string being searched
+		dataDump.seekg(fpos);                       // Seek to current file position (position after last match)
+		while (block.size() < blocksize) {          // While string size is less than the block size
+			dataDump.read(buffer, sizeof(buffer));
+			block.append(buffer, sizeof(buffer));
+		}
+	
+		fpos += parse(block, "<page>", "</page>", pages);
+		
+	}
+	return pages;
+}
 
+int main(int argc, char** argv) {
 
-
-
-
-
-
-
+	// Dump filename
+	string filename = "enwiki-20151201-pages-articles.xml";
+	
+	// Get vector of raw page strings
+	cout<<"Getting raw page strings..."<<endl;
+	
+	// WARNING -- RAM size requirements is about 1 GB per 30,000 articles
+	int npages = 30000;
+	vector<string> raw_pages = getPages(filename, npages);
+	
+	// Vector of wikipage objects
+	vector<wikiPage> pages;
+	
+	// Populate pages with initialized wikiPages
+	cout<<"Parsing raw page strings..."<<endl;
+	for (string i : raw_pages) {
+		wikiPage x(i);
+		if (not x.ns and not x.isRedirect) {
+			pages.push_back(x);
+		}
+	}
+	
+	return 0;
+	
+}
