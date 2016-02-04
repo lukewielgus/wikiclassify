1c12134acb647bcb383d63b8da012844d02afc3d
Nathan Kjer <nmkjer+git@gmail.com>
2016-02-04 13:41:32 -0500

    Moved word_frequency

--- fetch/data/speeches/speeches.py
+++ fetch/data/speeches/speeches.py
@@ -1,7 +1,8 @@
-import os
+import os, re
 
 for folder in ['obama']:
 	for x in os.listdir(folder):
 		if x.endswith('.txt'):
 			with open('%s/%s' % (folder, x)) as f:
-				speech = f.read()
+				speech = re.sub(r'([^\s\w])+', '', f.read().lower())
+				print speech
